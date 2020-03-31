5b00955935f03b5faf316d7bd167771e2d6a5fd2
Jared Glaser <Jared@glaserfamily.net>
2020-03-31 19:53:31 -0400

    game no longer prematurely ends

--- Engine.js
+++ Engine.js
@@ -4,33 +4,34 @@ class Engine {
         this.lasttimestamp = 0.0;
         this.logicController = logicController;
     }
-    
+
     init() {
         var engineinstance = this;
-        requestAnimationFrame(function(timestamp){
+        requestAnimationFrame(function (timestamp) {
             engineinstance.logicController.gameloop(timestamp, engineinstance);
         });
     }
 
-    boundsDetection(entity){
+    boundsDetection(entity) {
         //check out of bounds
         var element = document.getElementById(entity.id);
-        if(entity.y >= $("#container").height() + 100 ) {
-            if(entity.type == entityType.PLATFORM){
-                element.remove();
-                this.entities.splice(this.entities.indexOf(entity),1);
-            }
-            else {
+        if (entity.y >= $("#container").height() + 100) {
+            if (entity.type == entityType.PLAYER) {
                 for (var i = this.entities.length - 1; i > -1; i--) {
                     if (i != 0) {
                         document.getElementById(this.entities[i].id).remove();
                     }
-                    this.entities.splice(this.entities.indexOf(this.entities[i]),1);
+                    this.entities.splice(this.entities.indexOf(this.entities[i]), 1);
                 }
                 document.getElementById("GO-header").style.visibility = "visible";
                 this.logicController.gameOver = true;
             }
+            else {
+                element.remove();
+                this.entities.splice(this.entities.indexOf(entity), 1);
+            }
         }
+
         if (entity.type == entityType.PLAYER) {
             if (entity.x >= $("#container").width() - 57) {
                 entity.x = 0;
@@ -42,7 +43,7 @@ class Engine {
         else if (entity.type == entityType.ENEMY) {
             if (entity.x >= $("#container").width() + 100) {
                 element.remove();
-                this.entities.splice(this.entities.indexOf(entity),1);
+                this.entities.splice(this.entities.indexOf(entity), 1);
             }
         }
     }
